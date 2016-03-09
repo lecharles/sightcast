5714ea1021fc449748ad88238b2c1ba60abff518
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-03-08 19:36:53 -0500

    Upload avatar successful

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,6 @@ class User < ActiveRecord::Base
   validates :username, uniqueness: true
   validates :email, uniqueness: true
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :thumb => "100x100>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
