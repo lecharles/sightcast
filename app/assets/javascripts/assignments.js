961ff9d6e2ffa272a86303b24a0df5e20347849a
Cody Condon <codycondon50@gmail.com>
2016-03-20 17:04:27 -0400

    take out invalid css

--- app/assets/javascripts/assignments.js
+++ app/assets/javascripts/assignments.js
@@ -11,9 +11,9 @@ var CAMERA_ARRAY = [];
 var sightcastCall;
 
 
-AUTH_URL = 'https://sightcast.herokuapp.com/tokens/get_token?uid=';
+//AUTH_URL = 'https://sightcast.herokuapp.com/tokens/get_token?uid=';
 
-//AUTH_URL = 'http://localhost:3000/tokens/get_token?uid='
+AUTH_URL = 'http://localhost:3000/tokens/get_token?uid='
 
 
 function isCaster(sightcaster) {
